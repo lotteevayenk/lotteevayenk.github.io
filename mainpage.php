31244df87ed267048ffd79e9e83b1331b60da427
lotteevayenk <deinformaticasquad@gmail.com>
2020-03-05 15:54:59 +0100

    yannick save

--- mainpage.php
+++ mainpage.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Main page</title>
+    <title>Receptenvinder</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <link rel="icon" href="https://images.vexels.com/media/users/3/151613/isolated/preview/71680abcac6aa750c99fa2589a48998b-cherry-cake-slice-flat-icon-by-vexels.png" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
   </head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <body>
-    <p>test</p>
+    <div class="navbar">
+      <ul class="navig">
+        <li><a href="index.html"><b>Home</b></a></li>
+        <li><a href="mainpage.php">Receptenvinder</a></li>
+        <li><a href="database/phpliteadmin.php">SQL</a></li>
+      </ul>
+    </div>
+    <div class="topnav">
+      <input type="text" placeholder="Search..">
+    </div>
   </body>
 </html>
